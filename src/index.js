9c2c886f599b926d6b8cdef68b0350f8a5ebd815
Hamish Macpherson <hamstu@gmail.com>
2020-09-25 13:06:25 -0700

    update

--- src/index.js
+++ src/index.js
@@ -5,7 +5,6 @@ async function run() {
   const myToken = core.getInput("github-token");
   const octokit = github.getOctokit(myToken);
   const context = core.getInput("status-context");
-  const waitForState = core.getInput("wait-for-state");
   const timeout = core.getInput("timeout");
 
   const commit = await octokit.repos.getCommit(github.context.sha);
