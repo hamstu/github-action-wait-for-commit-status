f6bf34a11785dc0a3b50b450cd87e8afe8b3a87e
Hamish Macpherson <hamstu@gmail.com>
2020-09-26 14:26:36 -0700

    wait for status first pass

--- dist/index.js
+++ dist/index.js
@@ -5779,29 +5779,49 @@ function wrappy (fn, cb) {
 
 const core = __webpack_require__(127);
 const github = __webpack_require__(134);
+const { stat } = __webpack_require__(747);
+
+const myToken = core.getInput("github-token", { required: true });
+const octokit = github.getOctokit(myToken);
+const statusContext = core.getInput("status-context", { required: true });
+const checkInterval = Number(core.getInput("check-interval")) || 10000;
+
+let timer = null;
+
+async function getCurrentCommitStatus({ context }) {
+  let statuses = [];
+  try {
+    const response = await octokit.repos.listCommitStatusesForRef({
+      owner: github.context.payload.repository.owner.login,
+      repo: github.context.payload.repository.name,
+      ref: github.context.payload.head_commit.id,
+    });
+    if (response) {
+      statuses = response.data;
+    }
+  } catch (error) {
+    return null;
+  }
+  const status = statuses.find((s) => s.context === context);
+  return status;
+}
+
+async function check({ context }) {
+  const status = await getCurrentCommitStatus({ context });
+  if (status) {
+    if (status.state === "failure") {
+      core.setFailed(`Status ${status.context} failed`);
+      process.exit(1);
+    } else if (status.state === 'success') {
+      core.setOutput("status", status);
+      process.exit(0);
+    }
+  }
+  timer = setTimeout(check.bind(null, { context }), checkInterval);
+}
 
 async function run() {
-  const myToken = core.getInput("github-token");
-  const octokit = github.getOctokit(myToken);
-  const context = core.getInput("status-context");
-  const timeout = core.getInput("timeout");
-
-  // const commit = await octokit.repos.getCommit({
-  //   owner: github.context.payload.repository.owner.login,
-  //   repo: github.context.payload.repository.name,
-  //   sha: github.context.sha,
-  // });
-
-  // console.log(`commit ${github.context.sha}`);
-
-  // console.log("----------------------", "github.context", github.context);
-  const statuses = await octokit.repos.listCommitStatusesForRef({
-    owner: github.context.payload.repository.owner.login,
-    repo: github.context.payload.repository.name,
-    ref: github.context.payload.head_commit.id,
-  });
-
-  console.log(statuses);
+  check({ context: statusContext });
 }
 
 run();
--- src/index.js
+++ src/index.js
@@ -1,28 +1,48 @@
 const core = require("@actions/core");
 const github = require("@actions/github");
+const { stat } = require("fs");
+
+const myToken = core.getInput("github-token", { required: true });
+const octokit = github.getOctokit(myToken);
+const statusContext = core.getInput("status-context", { required: true });
+const checkInterval = Number(core.getInput("check-interval")) || 10000;
+
+let timer = null;
+
+async function getCurrentCommitStatus({ context }) {
+  let statuses = [];
+  try {
+    const response = await octokit.repos.listCommitStatusesForRef({
+      owner: github.context.payload.repository.owner.login,
+      repo: github.context.payload.repository.name,
+      ref: github.context.payload.head_commit.id,
+    });
+    if (response) {
+      statuses = response.data;
+    }
+  } catch (error) {
+    return null;
+  }
+  const status = statuses.find((s) => s.context === context);
+  return status;
+}
+
+async function check({ context }) {
+  const status = await getCurrentCommitStatus({ context });
+  if (status) {
+    if (status.state === "failure") {
+      core.setFailed(`Status ${status.context} failed`);
+      process.exit(1);
+    } else if (status.state === 'success') {
+      core.setOutput("status", status);
+      process.exit(0);
+    }
+  }
+  timer = setTimeout(check.bind(null, { context }), checkInterval);
+}
 
 async function run() {
-  const myToken = core.getInput("github-token");
-  const octokit = github.getOctokit(myToken);
-  const context = core.getInput("status-context");
-  const timeout = core.getInput("timeout");
-
-  // const commit = await octokit.repos.getCommit({
-  //   owner: github.context.payload.repository.owner.login,
-  //   repo: github.context.payload.repository.name,
-  //   sha: github.context.sha,
-  // });
-
-  // console.log(`commit ${github.context.sha}`);
-
-  // console.log("----------------------", "github.context", github.context);
-  const statuses = await octokit.repos.listCommitStatusesForRef({
-    owner: github.context.payload.repository.owner.login,
-    repo: github.context.payload.repository.name,
-    ref: github.context.payload.head_commit.id,
-  });
-
-  console.log(statuses);
+  check({ context: statusContext });
 }
 
 run();
